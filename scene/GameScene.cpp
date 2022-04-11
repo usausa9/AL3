37245a5e6edb07ea294552a357e339c8e76c2999
usausa9 <k021g1269@g.neec.ac.jp>
2022-04-11 15:38:48 +0900

    02-01 とりあえず完成？

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,9 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete model_; 
+}
 
 void GameScene::Initialize() {
 
@@ -14,6 +16,30 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	// テクスチャの読み込み
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクション
+	viewProjection_.Initialize();
+
+	// X,Y,Z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+
+	// X,Y,Z 軸周りの回転角を設定
+	/*worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};*/
+	worldTransform_.rotation_ = {XMConvertToRadians(45.0f), XMConvertToRadians(45.0f), 0.0f};
+
+	// X,Y,Z 軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f,10.0f, 10.0f};
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
 }
 
 void GameScene::Update() {}
@@ -44,6 +70,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
