ba4c2163c6fea786438b6366536f286b85ff40ad
usausa9 <k021g1269@g.neec.ac.jp>
2022-04-11 15:03:32 +0900

    01-02 確認課題 完成

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -48,6 +48,29 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0; //まりお
+
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0; //sad
+
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0; // sad
+
+	// 値を表示したい変数
+	int32_t value_ = 0;
+
+	// スプライト
+	Sprite* sprite_ = nullptr;
+
+	// 3Dモデル
+	Model* model_ = nullptr;
+
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
